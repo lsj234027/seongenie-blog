eb4348aa717f65e489ea96d5b27acf4c0b8965da
seongenie <lsj234027@gmail.com>
2019-07-07 14:06:45 +0900

    초기설정

---
+++ content/blog/development/new-post.md
@@ -0,0 +1,7 @@
+---
+title: New post
+date: 2019-07-07 12:07:09
+category: development
+---
+
+## 1. Test Md file
--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ const metaConfig = require('./gatsby-meta-config')
 
 module.exports = {
   siteMetadata: metaConfig,
+  pathPrefix: `/blog`,
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
